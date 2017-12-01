42bb66fc2e09bb1a898277e062c5cb79090ea182
John Ellis <john@seriouslyawesome.com>
2017-12-01 12:55:10 -0600

    Add dependencies and requirements.

--- lib/postmark_mailer.rb
+++ lib/postmark_mailer.rb
@@ -1,4 +1,7 @@
-require "postmark_mailer/version"
+require 'postmark_mailer/version'
+require 'postmark_mailer/base'
+require 'postmark_mailer/delivery_job'
+require 'postmark_mailer/message_delivery'
 
 module PostmarkMailer
   # Your code goes here...
--- lib/postmark_mailer/delivery_job.rb
+++ lib/postmark_mailer/delivery_job.rb
@@ -1,5 +1,7 @@
+require "active_job"
+
 module PostmarkMailer
-  class DeliveryJob < ApplicationJob
+  class DeliveryJob < ActiveJob::Base
     queue_as { ActionMailer::Base.deliver_later_queue_name }
 
     def perform(options)
