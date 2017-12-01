eb2ed28409bc084bba395a03187edb968f306a13
John Ellis <john@seriouslyawesome.com>
2017-12-01 13:27:29 -0600

    Use consistent naming.

--- lib/postmark_mailer/configuration.rb
+++ lib/postmark_mailer/configuration.rb
@@ -1,10 +1,10 @@
 module PostmarkMailer
   class Configuration
-    attr_accessor :api_key, :delivery_queue
+    attr_accessor :api_key, :default_delivery_queue
 
     def initialize
       @api_key = nil
-      @delivery_queue = :default
+      @default_delivery_queue = :default
     end
   end
 end
--- lib/postmark_mailer/delivery_job.rb
+++ lib/postmark_mailer/delivery_job.rb
@@ -2,7 +2,7 @@ require "active_job"
 
 module PostmarkMailer
   class DeliveryJob < ActiveJob::Base
-    queue_as { PostmarkMailer.configuration.delivery_queue }
+    queue_as { PostmarkMailer.configuration.default_delivery_queue }
 
     def perform(options)
       MessageDelivery.new(options).deliver_now
